f6b5a124ecf7aaef50b864b27d183448491664ad
projintegrador2023 <127967558+projintegrador2023@users.noreply.github.com>
2023-08-15 09:00:46 -0300

    passo 4 a 8

--- app/src/main/java/nunes/davi/galeriapublica/MainActivity.java
+++ app/src/main/java/nunes/davi/galeriapublica/MainActivity.java
@@ -1,14 +1,125 @@
 package nunes.davi.galeriapublica;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
+    static int RESULT_REQUEST_PERMISSION = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+
+        bottomNavigationView = findViewById(R.id.btNav);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() { //detecta qual item foi selecionado
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                vm.setNavigationOpSelected(item.getItemId());
+                switch (item.getItemId()){
+                    case R.id.gridViewOp: //se o item for o de grid
+                        GridViewFragment gridViewFragment = GridViewFragment.newInstance();
+                        setFragment(gridViewFragment); //seta o fragment de grid na tela
+                        break;
+                    case R.id.listViewOp: //se o item for o de lista
+                        ListViewFragment listViewFragment = ListViewFragment.newInstance();
+                        setFragment(listViewFragment); //seta o fragment de lista na tela
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragContainer, fragment); //substitui o fragmento na tela
+        fragmentTransaction.addToBackStack(null); //esse fragmento agora faz parte da pilha de tela do botão voltar do Android
+        fragmentTransaction.commit();
+    }
+
+    protected void onResume(){
+        super.onResume();
+        List<String> permissions = new ArrayList<>();
+        permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        checkForPermissions(permissions);
+    }
+
+    private void checkForPermissions(List<String> permissions){
+        List<String> permissionsNotGranted = new ArrayList<>();
+
+        for (String permission : permissions){
+            if (!hasPermission(permission)){
+                permissionsNotGranted.add(permission);
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (permissionsNotGranted.size() > 0){
+                requestPermissions(permissionsNotGranted.toArray(new String[permissionsNotGranted.size()]), RESULT_REQUEST_PERMISSION);
+            }
+            else {
+                MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+                int navigationOpSelected = vm.navigationOpSelected;
+                bottomNavigationView.setSelectedItemId(navigationOpSelected);
+            }
+        }
+    }
+
+    private boolean hasPermission(String permission){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            return ActivityCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_GRANTED;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        final List<String> permissionsRejected = new ArrayList<>();
+        if (requestCode == RESULT_REQUEST_PERMISSION){
+            for (String permission : permissions){
+                if (!hasPermission(permission)){
+                    permissionsRejected.add(permission);
+                }
+            }
+        }
+        if (permissionsRejected.size() > 0){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))){
+                    new AlertDialog.Builder(MainActivity.this).setMessage("Para usar esse app é preciso conceder essas permissões").setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int which) {
+                            requestPermissions(permissionsRejected.toArray(new String[permissionsRejected.size()]), RESULT_REQUEST_PERMISSION);
+                        }
+                    }).create().show();
+                }
+            }
+        }
+        else {
+            MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+            int navigationOpSelected = vm.navigationOpSelected;
+            bottomNavigationView.setSelectedItemId(navigationOpSelected);
+        }
     }
 }
